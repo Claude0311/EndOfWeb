c809a0b78e20c35de8d894f8c234004f49347033
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-09-21 23:15:34 +0800

    rm body-parser, use express.json() instead

--- index.js
+++ index.js
@@ -2,38 +2,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const cors = require('cors')
 const mongoose = require('./backend/routes/Schemas/db')
 
 mongoose.connection.on('open', () => {
   console.log('DB on')
-  const bodyParser = require('body-parser')
   const session = require('express-session')
   const MongoStore = require('connect-mongo')(session)
+
   //post, get時的解碼
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(bodyParser.json())
-  // app.use(function(req, res, next) {
-  // 	res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-  // 	res.header("Access-Control-Allow-Credentials", true);
-  // 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // 	res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
-  // 	next();
-  // });
-  // app.use(
-  //   cors({
-  //     origin: 'http://localhost:3000',
-  //     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'PATCH', 'DELETE'],
-  //     credentials: true,
-  //   }),
-  // )
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+
   //session 設定
   //參考網站https://www.cnblogs.com/chyingp/p/nodejs-learning-express-session.html
   app.use(
     session({
       name: 'eeplus',
       secret: 'fuewhzk', // 用来對session id相關的cookie進行簽名，建議128byte亂碼
-      //store: new FileStore({ logFn: function () {} }), // 本地儲存session（文本文件，也可以選擇其他store，比如redis的）
       store: new MongoStore({
         mongooseConnection: mongoose.connection,
       }),
@@ -46,8 +31,8 @@ mongoose.connection.on('open', () => {
     }),
   )
 
+  //backend
   app.use('/api', require('./backend/routes/api'))
-  // })
 
   //frontend
   if (!process.env.HERO) {
@@ -66,14 +51,6 @@ mongoose.connection.on('open', () => {
       res.sendFile(HTML_FILE) // EDIT
     })
   }
-  //old frontend
-  //Serve static files from the React app
-  //詳細資訊看：https://expressjs.com/zh-tw/starter/static-files.html
-  // app.use(express.static(path.join(__dirname, 'client/build')))
-  /*app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html')); //這個缺點是react build的index不是我們寫的那個
-  //res.redirect('/'); //這個按F5會亂跳，先捨棄
-});*/
 
   //server on
   app.listen(process.env.PORT || 1993, () => {
